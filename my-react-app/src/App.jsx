d3e49e4f64bfe516ba0cbdb9b53de4becffb3164
JoelOccius <143296252+JoelOccius@users.noreply.github.com>
2025-04-03 21:57:55 -0400

    sixth commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,8 @@
+
+export default function App() {
+  return (
+    <div className="p-10 bg-gray-100 text-center">
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind + Vite!</h1>
+    </div>
+  )
+}
